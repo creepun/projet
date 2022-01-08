149672bf568e0cf03a9c6afb3b0f4ac4aecd93cc
Mercier Mickael <testmail@yahoo.fr>
2022-01-09 00:45:22 +0100

    ajout dataDixture formation

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,9 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use APP\Entity\Entreprise;
+use APP\Entity\Formation;
+use APP\Entity\Stage;
 
 class AppFixtures extends Fixture
 {
@@ -11,6 +14,22 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+        
+        $faker = \Faker\Factory::create('fr_FR');
+
+        $stage= new TypeRessource();
+        $stage = setNomType("stage");
+        
+        $entreprise= new TypeRessource();
+        $entreprise = setNomType("entreprise");
+        
+        $DUTInfo = new Formation();
+        $DUTInfo->setNomCourt("DUT INFO");
+        $DUTInfo->setNomLong("DUT Informatique");
+       
+        $DUTGea = new Formation();
+        $DUTGea->setNomCourt("DU TIC");
+        $DUTGea->setNomLong("DU Technologies de l'Information et de la Communication");
 
         $manager->flush();
     }
