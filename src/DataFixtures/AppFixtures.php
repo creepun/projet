bcd19b285a7597349baf1d2fb78f10c7dc0969bc
Mercier Mickael <testmail@yahoo.fr>
2022-01-09 01:46:19 +0100

    ajout datafixture formation

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -32,15 +32,42 @@ class AppFixtures extends Fixture
             $manager->persist($tabFormations);
         }
 
-        $nbRessourcesAGenerer = $faker->numberBetween($min = 4, $max = 25);
-        for ($numRessource=0; $numRessource < $nbRessourcesAGenerer; $numRessource++) {
+        $nbEntrepriseAGenerer = $faker->numberBetween($min = 4, $max = 25);
+        for ($numRessource=0; $numRessource < $nbEntrepriseAGenerer; $numRessource++) {
             $entreprise = new Entreprise();
             $entreprise = setNom($faker->company);
             $entreprise = setActivité($faker->realText($maxNbChars = 200, $indexSize = 2));
             $entreprise = setAdresse($faker->address);
             $entreprise = setURLSite($faker->domainName);
+            
+            $nbStageAGenerer = $faker->numberBetween($min = 0, $max = 10);
+            for ($numRessource=0; $numRessource < $nbStageAGenerer; $numRessource++){
+                $stage = new Stage();
+                $stage = setTitre($faker->jobTitle);
+                $stage = setDescription($faker->realText($maxNbChars = 200, $indexSize = 2));
+                $stage = setEmail($faker->email);
+                $stage = addEntreprise($entreprise);
+                
+                $numFormation = $faker->numberBetween($min = 1, $max = 3);
+                switch ($numFormation){
+                    case 1:
+                        $stage->addFormation($DUTInfo);
+                        $$DUTInfo->addStage($stage);
+                        break;
+                    case 2:
+                        echo "i égal 1";
+                        break;
+                    case 3:
+                        echo "i égal 2";
+                        break;
+                }
+            }
+            $entreprise->addStage($stage);
+            $manager->persist($stage);
         }
+        
         $manager->persist($entreprise);
+        
         $manager->flush();
     }
 }
